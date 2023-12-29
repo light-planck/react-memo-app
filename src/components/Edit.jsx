a7b0a50800df4b35428fefc083b4eb87487f59d5
light-planck <seseifunc16@gmail.com>
2023-12-29 18:42:37 +0900

    feat: 編集ページの見た目を作成

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export const Edit = () => {
-  return <div>edit</div>;
+  return (
+    <div className="edit">
+      <textarea className="edit-textarea" />
+      <div className="button-container">
+        <button className="large-button">編集</button>
+        <button className="small-button">削除</button>
+      </div>
+    </div>
+  );
 };
