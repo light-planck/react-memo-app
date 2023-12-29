0fee7be885f0e155dad127e2123722dd8cbdcfe0
light-planck <seseifunc16@gmail.com>
2023-12-29 16:23:07 +0900

    feat: アプリの雛形を作成

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import { List } from "./components/List";
+import { Edit } from "./components/Edit";
+
+function App() {
+  return (
+    <div>
+      <List />
+      <Edit />
+    </div>
+  );
+}
+
+export default App;
