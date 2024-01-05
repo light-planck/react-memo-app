2851e7984613f53dca7142c2e43a0b74a578bdf5
light-planck <seseifunc16@gmail.com>
2024-01-05 14:26:26 +0900

    fix: local storageを修正

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -6,21 +6,20 @@ export const useLocalStorage = (key, initialValue) => {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
-      console.log(error);
-      return initialValue;
+      throw new Error(`Could not retrieve localStorage item: ${error}`);
     }
   });
 
-  const setValue = (value) => {
+  const setLocalStorageValue = (value) => {
     try {
       const valueToStore =
         value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
       window.localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
-      console.log(error);
+      throw new Error(`Could not set localStorage item: ${error}`);
     }
   };
 
-  return { storedValue, setValue };
+  return { storedValue, setLocalStorageValue };
 };
