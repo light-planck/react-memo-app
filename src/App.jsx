283eb15ee1612559ebc45ff78281bb0f14d68d0a
light-planck <seseifunc16@gmail.com>
2024-01-05 15:00:24 +0900

    fix: appを修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 import { List } from "./components/List";
 import { Edit } from "./components/Edit";
 import { useViewMode } from "./hooks/useViewMode";
-import { useLocalStorage } from "./hooks/userLocalStorage";
 
 function App() {
-  const { viewMode, viewHeader, toggleViewMode } = useViewMode({
-    modes: ["list", "edit"],
-    headers: ["一覧", "編集"],
-  });
-
-  const { storedValue: memos, setValue } = useLocalStorage("memos", []);
+  const { viewMode, viewHeader, toggleViewMode } = useViewMode();
+  const [selectedId, setSelectedId] = useState(null);
 
   return (
     <div className="container">
       <h1>{viewHeader}</h1>
       <div className="app">
-        <List
-          items={memos}
-          toggleViewMode={toggleViewMode}
-          setValue={setValue}
-        />
-        {viewMode === "edit" && <Edit />}
+        <List setSelectedId={setSelectedId} toggleViewMode={toggleViewMode} />
+        {viewMode === "edit" && (
+          <Edit
+            selectedId={selectedId}
+            setSelectedId={setSelectedId}
+            toggleViewMode={toggleViewMode}
+          />
+        )}
       </div>
     </div>
   );
