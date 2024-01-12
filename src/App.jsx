bdfc07a8715823d4c1a058ca7e8bc23397684489
light-planck <seseifunc16@gmail.com>
2024-01-12 17:30:38 +0900

    refactor: memoとその操作をカスタムフックスとして切り出す

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,25 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React from "react";
 
 import "./App.css";
 
 import { List } from "./components/List";
 import { Edit } from "./components/Edit";
-import { useViewMode } from "./hooks/useViewMode";
-import { useLocalStorage } from "./hooks/useLocalStorage";
+import { useMemoManagement } from "./hooks/useMemoManagement";
 
 const App = () => {
-  const { viewMode, toggleViewMode } = useViewMode();
-  const [selectedId, setSelectedId] = useState("");
-  const { storedValue: memos, setLocalStorageValue: setMemos } =
-    useLocalStorage("memos", []);
+  const {
+    memos,
+    selectedId,
+    viewMode,
+    toggleMemo,
+    addMemo,
+    editMemo,
+    deleteMemo,
+    toggleViewMode,
+  } = useMemoManagement();
 
   const title = viewMode === "list" ? "一覧" : "編集";
   const selectedMemo = memos.find((memo) => memo.id === selectedId);
-
-  const addMemo = () => {
-    const newMemo = {
-      id: uuidv4(),
-      title: "新規メモ",
-      content: "新規メモ",
-    };
-    setMemos([...memos, newMemo]);
-
-    toggleMemo(newMemo.id);
-  };
-
-  const editMemo = (id, text) => {
-    setMemos(
-      memos.map((memo) =>
-        memo.id === id
-          ? { id: id, title: text.split("\n")[0], content: text }
-          : memo
-      )
-    );
-  };
-
-  const toggleMemo = (id) => {
-    setSelectedId(id);
-    if (viewMode === "list") toggleViewMode();
-  };
-
-  const deleteMemo = (id) => {
-    setMemos(memos.filter((memo) => memo.id !== id));
-  };
 
   return (
     <div className="container">
---
+++ src/hooks/useMemoManagement.js
@@ -0,0 +1,53 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { useLocalStorage } from "./useLocalStorage";
+import { useState } from "react";
+import { useViewMode } from "./useViewMode";
+
+export const useMemoManagement = () => {
+  const { storedValue: memos, setLocalStorageValue: setMemos } =
+    useLocalStorage("memos", []);
+  const [selectedId, setSelectedId] = useState("");
+  const { viewMode, toggleViewMode } = useViewMode();
+
+  const toggleMemo = (id) => {
+    setSelectedId(id);
+    if (viewMode === "list") toggleViewMode();
+  };
+
+  const addMemo = () => {
+    const newMemo = {
+      id: uuidv4(),
+      title: "新規メモ",
+      content: "新規メモ",
+    };
+    setMemos([...memos, newMemo]);
+
+    toggleMemo(newMemo.id);
+  };
+
+  const editMemo = (id, text) => {
+    setMemos(
+      memos.map((memo) =>
+        memo.id === id
+          ? { id: id, title: text.split("\n")[0], content: text }
+          : memo
+      )
+    );
+  };
+
+  const deleteMemo = (id) => {
+    setMemos(memos.filter((memo) => memo.id !== id));
+  };
+
+  return {
+    memos,
+    selectedId,
+    viewMode,
+    toggleMemo,
+    addMemo,
+    editMemo,
+    deleteMemo,
+    toggleViewMode,
+  };
+};
